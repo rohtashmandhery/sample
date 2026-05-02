ddab043c8b35de2b18ef382c3b8ee3214fdf1064
rohtashmandhery <61076907+rohtashmandhery@users.noreply.github.com>
2026-05-02 14:40:21 +0530

    remote change

--- lifecycle/app/src/main/java/com/rohtash/lifecycle/life_cycle/MainActivity.java
+++ lifecycle/app/src/main/java/com/rohtash/lifecycle/life_cycle/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.rohtash.lifecycle.R;
-
+//remote chnage
 
 public class MainActivity extends AppCompatActivity {
     TextView helloTV;
